4dcaf4a0220543b145822b5359fa865cc60e4f20
kylemanalaysay <112703214+kylemanalaysay@users.noreply.github.com>
2023-05-24 17:54:04 +0800

    Update slit.py

--- slit.py
+++ slit.py
@@ -31,7 +31,7 @@ else:
     st.image(image,use_column_width=True)
     # Make prediction
     result = image_processing(image)
-    classes = [ 0:'Speed limit (20km/h)',
+    classes = { 0:'Speed limit (20km/h)',
             1:'Speed limit (30km/h)',
             2:'Speed limit (50km/h)',
             3:'Speed limit (60km/h)',
@@ -73,7 +73,7 @@ else:
             39:'Keep left',
             40:'Roundabout mandatory',
             41:'End of no passing',
-            42:'End no passing vehicle > 3.5 tons' ]
+            42:'End no passing vehicle > 3.5 tons' }
     string="Predicted Traffic🚦Sign is: "+ classes[np.argmax(result)]
     
     st.success(string)
